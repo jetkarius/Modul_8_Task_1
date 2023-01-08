2a50838bd5a36505bab2eb68c5e92d12a1791181
boroksid@yandex.ru <boroksid@yandex.ru>
2023-01-08 22:11:56 +0300

    Добавьте файлы проекта.

---
+++ Modul_8_Task_1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace Modul_8_Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите полный адрес очищаемой папки");
+            string sourceDirectory = Console.ReadLine();
+            if (Directory.Exists(sourceDirectory))
+            {
+                var di = new DirectoryInfo(sourceDirectory);
+                foreach (FileInfo file in di.GetFiles())
+                {
+                    if (true)
+                    {
+                        TimeSpan.FromMinutes(30);
+                        file.Delete();
+                        Console.WriteLine("Файл удалён");
+                    }
+                }
+                foreach (DirectoryInfo dir in di.GetDirectories())
+                {
+                    if (true)
+                    {
+                        TimeSpan.FromMinutes(30);
+                        dir.Delete(true);
+                        Console.WriteLine("Папка удалена");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Папка не найдена. \nВведите корректный адрес папки");
+            }
+        }
+    }
+}
